8fe595092ef01f16a22200e1803e7ada27a0deee
rakib-utsho <rakibutsho1920@gmail.com>
2024-12-17 23:36:02 +0600

    api callling

---
+++ script.js
@@ -0,0 +1,23 @@
+let apiQuotes = [];
+
+// Show new Quote
+function newQuote(){
+    //pick random quotes from apiQuotes array
+    const quote =  apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+// Get Quotes from API
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    }catch (error) {
+        //catch error here
+    }
+}
+
+// Call function
+getQuotes();
